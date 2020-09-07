37983ce771652e223cec1037851c6cc354ed29c6
unknown <diego.resendez@yahoo.com>
2020-09-06 19:39:18 -0700

    Bug Fix

--- Develop/app.js
+++ Develop/app.js
@@ -12,7 +12,7 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const manager = new Manager();
-const employee = [];
+const employees = [];
 
 const managerQuestions = [
     {
@@ -150,6 +150,8 @@ function promptIntern()
         promptManager();
     });
 }
+
+init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
